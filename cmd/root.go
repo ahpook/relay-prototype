5e05dd326791f7a2fbef5b4943c3c20d030bc206
Eric Sorenson <eric.sorenson@puppetlabs.com>
2020-03-23 14:04:08 -0700

    Remove --config since we're not using it

--- cmd/root.go
+++ cmd/root.go
@@ -19,9 +19,6 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -58,14 +55,12 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	outputformat := "text"
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.relay.yaml)")
 	rootCmd.PersistentFlags().StringVar(&outputformat, "out", "", "output format: text, json (default is text)")
 	rootCmd.PersistentFlags().BoolP("yes", "y", false, "assume 'yes', avoiding confirmation prompts")
 	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "increase verbosity to aid troubleshooting")
@@ -75,30 +70,4 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".relay" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".relay")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
